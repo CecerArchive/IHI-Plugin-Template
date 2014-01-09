249afc604ff84d6a2a2ff37476ef34ddf81a8f28
Cecer <Chris@cecer1.com>
2014-01-09 17:05:42 +0000

    Initial Commit

---
+++ IHI Plugin Template/$safeprojectname$.cs
@@ -0,0 +1,31 @@
+﻿using IHI.Server;
+
+namespace IHI.Server.Plugins.$safeprojectname$
+{
+    public class $safeprojectname$ : Plugin
+    {
+        public override string Name
+        {
+            get
+            {
+                return "$projectname$";
+            }
+        }
+        
+        internal EventFirer EventFirer
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        ///   Called when the plugin is started.
+        /// </summary>
+        public override void Start(EventFirer eventFirer)
+        {
+            EventFirer = eventFirer;
+
+            // Here would be a good place to start...
+        }
+    }
+}
